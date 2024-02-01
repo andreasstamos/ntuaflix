47bc5956e57286c3f5bf2a6313f719fd46f25632
KostasPik <kostas.pikoulas2002@gmail.com>
2024-02-01 02:26:55 +0200

    fixed parses issues

--- back-end/input_tsv.py
+++ back-end/input_tsv.py
@@ -9,25 +9,25 @@ from utils import parse_title_basics, parse_title_ratings, parse_title_principal
 async def main():
     db = next(get_db())
 
-    async with aiofiles.open('truncated_data/truncated_title.basics.tsv', 'r') as afp:
+    async with aiofiles.open('truncated_data/truncated_title.basics.tsv', 'r', encoding='utf-8') as afp:
         await parse_title_basics(afp, db)
 
-    async with aiofiles.open('truncated_data/truncated_title.ratings.tsv', 'r') as afp:
+    async with aiofiles.open('truncated_data/truncated_title.ratings.tsv', 'r', encoding='utf-8') as afp:
         await parse_title_ratings(afp, db)
 
-    async with aiofiles.open('truncated_data/truncated_title.crew.tsv', 'r') as afp:
+    async with aiofiles.open('truncated_data/truncated_title.crew.tsv', 'r', encoding='utf-8') as afp:
         await parse_title_crew(afp, db)
 
-    async with aiofiles.open('truncated_data/truncated_title.akas.tsv', 'r') as afp:
+    async with aiofiles.open('truncated_data/truncated_title.akas.tsv', 'r', encoding='utf-8') as afp:
         await parse_title_akas(afp, db)
 
-    async with aiofiles.open('truncated_data/truncated_name.basics.tsv', 'r') as afp:
+    async with aiofiles.open('truncated_data/truncated_name.basics.tsv', 'r', encoding='utf-8') as afp:
         await parse_name_basics(afp, db)
 
-    async with aiofiles.open('truncated_data/truncated_title.episode.tsv', 'r') as afp:
+    async with aiofiles.open('truncated_data/truncated_title.episode.tsv', 'r', encoding='utf-8') as afp:
         await parse_title_episode(afp, db)
 
-    async with aiofiles.open('truncated_data/truncated_title.principals.tsv', 'r') as afp:
+    async with aiofiles.open('truncated_data/truncated_title.principals.tsv', 'r', encoding='utf-8') as afp:
         await parse_title_principals(afp, db)
     
 asyncio.run(main())
