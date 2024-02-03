0f8c1d1c63bdaf06a9c5da9dd7813f49bc4c9649
Andreas Stamos <stamos.aa@gmail.com>
2024-02-03 17:34:53 +0200

    cli testing: tests for person commands

---
+++ cli-client/tests/test_person.py
@@ -0,0 +1,29 @@
+from typer.testing import CliRunner
+import pytest
+import pathlib
+import json
+import random
+from cli import app
+from .utils import json_compare2
+
+random.seed(42)
+
+PERSON_SUBSTRING = "da"
+with open(pathlib.Path(__file__).parent / "testdata/people.json", "r") as f:
+    PEOPLE = json.load(f)
+
+runner = CliRunner(mix_stderr=False)
+
+@pytest.mark.parametrize("person", random.sample(PEOPLE, 5))
+def test_title(person):
+    result = runner.invoke(app, ['name', '--nameid', person['nameID']])
+    assert result.exit_code == 0, result.exit_code
+    assert not result.stderr
+    assert json_compare2(result.stdout, person)
+
+def test_searchname():
+    result = runner.invoke(app, ["searchname", '--namepart', PERSON_SUBSTRING])
+    assert result.exit_code == 0, result.exit_code
+    assert not result.stderr
+    assert json_compare2(result.stdout, PEOPLE)
+
