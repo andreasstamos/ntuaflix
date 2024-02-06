1b13ff8b4843091eec705632bd7aacbb81b84ccf
Andreas Stamos <stamos.aa@gmail.com>
2024-02-06 19:57:01 +0200

    Docker postgres preload data when init

--- back-end/db_backup.sh
+++ back-end/db_backup.sh
@@ -4,5 +4,5 @@ OUTPUT_SQL=db_dump.sql
 
 source .env
 
-PGPASSWORD=$DB_PASSWORD pg_dump -U $DB_USERNAME -d $DB_DATABASE -h $DB_HOST -c --if-exists >$OUTPUT_SQL
+PGPASSWORD=$DB_PASSWORD pg_dump -U $DB_USERNAME -d $DB_DATABASE -h $DB_HOST -c --if-exists --no-acl --no-owner >$OUTPUT_SQL
 
